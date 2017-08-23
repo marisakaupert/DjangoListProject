689fbb458a264e25d6402093049938ef31902fa6
marisaslozano <marisalozano1995@gmail.com>
2017-08-23 12:26:11 -0400

    write unti tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,49 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.template import loader
+from django.views import generic
+from djange.utils import timezone
+
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""
+		Return the last five published questions (not including
+		those set to be published in the future)
+		"""
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
-from django.http import HttpResponse
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# redisplay the question voting form
+		return render(request, 'polls/detail.html', {'question': question,
+		'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
 
-def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	# Always return an HttpResponseRedirect after successfully dealing with POST
+	# data. This prevents data from being posted twice if a user hits the Bak button
+	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
