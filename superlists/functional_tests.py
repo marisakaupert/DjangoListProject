d8b6764ef6f2e881beb8b5c31fa300c36f002b75
marisaslozano <marisalozano1995@gmail.com>
2017-08-29 16:10:09 -0400

    Fnctional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,21 +18,37 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # the user goes to the homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000/')
 
         # the page title and header say to-do
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # the user is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.getattribute('placeholder'),
+            'Enter a to-do item.'
+        )
 
         # the user types a task into a text-box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When the user hits enter, the page updates, and now the page lists
         # "1: " + the task they entered, as an item in the list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: buy peacock feathers' for row in rows)
+        )
 
         # There is another text box letting the user add another task.
         # the user enters another task
+        self.fail('Finish the test!')
 
         # The page updates and shows both items on the lists
 
