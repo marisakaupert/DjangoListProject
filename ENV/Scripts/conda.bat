5f3672058c15941aa2e1b787984df25e48db22fd
marisaslozano <marisalozano1995@gmail.com>
2017-09-13 12:49:03 -0400

    Delete old projects

--- ENV/Scripts/conda.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call "C:\Users\Marisa\Anaconda2\Scripts\conda" %*
--- ENV/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\marisa\documents\djangoprojects\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
